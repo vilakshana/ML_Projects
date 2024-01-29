9f3296a6faf2dbc2022b8aa3c19e43fc04327920
Vilakshana <vilakshanajha@gmail.com>
2024-01-29 21:16:29 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,7 +16,7 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
+
 '''if __name__=="__main__":
     try: 
         a=1/0
